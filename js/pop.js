2a547836df74a0148478fac85cb26bee51e6448c
kevincampero42@gmail.com <kevincampero42@gmail.com>
2021-02-21 05:56:23 -0400

    act

---
+++ js/pop.js
@@ -0,0 +1,12 @@
+function popupToggle(){
+    let popup =document.getElementById('popup');
+    popup.classList.toggle('acti')
+}
+function popupToggle2(){
+    let popup =document.getElementById('popup2');
+    popup.classList.toggle('acti')
+}
+function popupToggle3(){
+    let popup =document.getElementById('popup3');
+    popup.classList.toggle('acti')
+}
